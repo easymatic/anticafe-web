2692f89aad4cc806dde55ed3de342b1bc2f60c00
Anton Pchelkin <imanbee@gmail.com>
2017-04-05 13:01:49 +0300

    Fix comma

--- src/app/persons/person.ts
+++ src/app/persons/person.ts
@@ -2,7 +2,7 @@ export class Person {
   constructor(
     public id: number,
     public card: number,
-    public first_name: string
+    public first_name: string,
     public last_name: string
   ) { }
 }
